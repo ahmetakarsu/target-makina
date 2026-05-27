6e390af953f0bcd2e0b04d264c04e95368674426
Ahmet AKARSU <ahmet_akarsu@hotmail.com>
2026-05-27 18:11:51 +0300

    Revert 'Remove hizmetler pages and links'

--- check_css.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("public/css/base.css", "r", encoding="utf-8") as f:
-    css = f.read()
-
-print("img in base.css:")
-matches = re.findall(r'(img\s*\{[^}]+\})', css)
-for m in matches:
-    print(m)
